c0f7d69f213bdb5e8089bd11f073768ce92fe42c
Evan <echin@bix.channelwood>
2019-03-29 11:14:08 -0400

    Saving crap

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -7,16 +7,22 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-GUID=$1
-REPO=$2
-CLUSTER=$3
+#GUID=$1
+GUID=eac
+#REPO=$2
+REPO=https://github.com/echin3/ocp_tasks_homework.git
+#CLUSTER=$3
+CLUSTER=master.na311.openshift.opentlc.com
+
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
 # Set up Jenkins with sufficient resources
 # TBD
+$(oc new-app jenkins-persistent -p ENABLE_OAUTH=true -p MEMORY_LIMIT=2Gi -p VOLUME_CAPACITY=4Gi -p DISABLE_ADMINISTRATIVE_MONITORS=true)
 
 # Create custom agent container image with skopeo
 # TBD
+$(oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\nUSER root\nRUN yum install -y skopeo && yum clean all\nUSER 1001' --name=jenkins-agent-appdev -n eac-jenkins)
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
 # TBD
